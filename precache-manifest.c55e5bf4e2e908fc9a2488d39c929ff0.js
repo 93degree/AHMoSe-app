73bf32f937c3f671b41c7eb81afdee43b4d35f8a
Diego Rojo <rojo@hey.com>
2021-04-16 15:49:30 +0200

    Updates

---
+++ precache-manifest.c55e5bf4e2e908fc9a2488d39c929ff0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "deb929c57ef40d64f8fc",
+    "url": "/AHMoSe-app/static/css/main.9ac02f97.chunk.css"
+  },
+  {
+    "revision": "deb929c57ef40d64f8fc",
+    "url": "/AHMoSe-app/static/js/main.40292522.chunk.js"
+  },
+  {
+    "revision": "4251f0f9935d1d661b1b",
+    "url": "/AHMoSe-app/static/js/runtime~main.b175d55d.js"
+  },
+  {
+    "revision": "b1c0ca261e1376228cc1",
+    "url": "/AHMoSe-app/static/css/2.292ac00a.chunk.css"
+  },
+  {
+    "revision": "b1c0ca261e1376228cc1",
+    "url": "/AHMoSe-app/static/js/2.6e9b1636.chunk.js"
+  },
+  {
+    "revision": "1ce84434cd88c69ba1cdb8edd07173a6",
+    "url": "/AHMoSe-app/static/media/features_info.1ce84434.csv"
+  },
+  {
+    "revision": "ceaf44b3e5e6c8d7da562a59344134bb",
+    "url": "/AHMoSe-app/static/media/knowledge_intervals.ceaf44b3.csv"
+  },
+  {
+    "revision": "5fecf2c80140c3c741f288b7158b2321",
+    "url": "/AHMoSe-app/static/media/models_data.5fecf2c8.csv"
+  },
+  {
+    "revision": "9658bd2b48e473d14094d126e383d432",
+    "url": "/AHMoSe-app/static/media/models_info.9658bd2b.csv"
+  },
+  {
+    "revision": "89b36add8386dcba1a10ee202117284f",
+    "url": "/AHMoSe-app/static/media/features_info.89b36add.csv"
+  },
+  {
+    "revision": "07106c22ae2d4c5445898eccc507b0f6",
+    "url": "/AHMoSe-app/static/media/models_data.07106c22.csv"
+  },
+  {
+    "revision": "6c90e31df841f58764b74fc1d57ccc8d",
+    "url": "/AHMoSe-app/static/media/models_info.6c90e31d.csv"
+  },
+  {
+    "revision": "c322f0ed97697c443e3fe94ce92709ca",
+    "url": "/AHMoSe-app/index.html"
+  }
+];
